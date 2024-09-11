14927be1703842bff5c67c1baf2556f08504c4d4
LanDev01 <alnjr0001@outlook.com>
2024-09-11 12:50:14 -0300

    Feat: criando rota de listar metas concluídas durante a semana separadamente

--- src/http/server.ts
+++ src/http/server.ts
@@ -8,15 +8,15 @@ import {
 import { createGoalRoute } from './routes/create-goal';
 import { createCompletionRoute } from './routes/create-completion';
 import { getPendingGoalsRoute } from './routes/get-pending-goals';
+import { getWeekSummaryRoute } from './routes/get-week-summary';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(getPendingGoalsRoute);
-
+app.register(getWeekSummaryRoute);
 app.register(createGoalRoute);
-
 app.register(createCompletionRoute);
 
 app
